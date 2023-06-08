9dc34052910f3c1848faf70f3c2b4f5d4ddb09ba
Ihor <skogrv@gmail.com>
2023-06-08 15:11:12 +0300

    feat: add sign up form validation

--- php/process-signup.php
+++ php/process-signup.php
@@ -1,3 +1,18 @@
 <?php
-    print_r($_POST);
+if (empty($_POST["name"])) {
+    die("Name is required");
+}
+
+if (strlen($_POST["password"]) < 8) {
+    die("Password should be 8 or more characters");
+}
+
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    die("Passwords should match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+var_dump($password_hash);
+
+print_r($_POST);
 ?>
