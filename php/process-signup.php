0582501797dc995827fa76f369d28cda78844428
Ihor <skogrv@gmail.com>
2023-06-08 16:15:40 +0300

    feat: add post-redirect-get pattern after sign-up

--- php/process-signup.php
+++ php/process-signup.php
@@ -18,14 +18,15 @@ $sql = "INSERT INTO user (username, password)
         VALUES (?, ?)";
 
 $stmt = $mysqli->stmt_init();
-if (! $stmt->prepare($sql) ) {
+if (!$stmt->prepare($sql)) {
     die("SQL error: " . $mysqli->error);
 }
 
 $stmt->bind_param("ss", $_POST["name"], $password_hash);
 
 if ($stmt->execute()) {
-    print_r("Registered");
+    header("Location: ../home.html");
+    exit;
 } else {
     if ($mysqli->errno === 1062) {
         die("Username already exists");
